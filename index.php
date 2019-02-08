1a3d113a803d9c6419992aa912eefe404fcf5e57
Israel Lopez H <Israellh2001@hotmail.com>
2019-02-08 10:03:35 -0600

    Slider
    Codigo del slider

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,900" rel="stylesheet">
+    <link rel="stylesheet" href="./css/main2.css">
+    <title>Login</title>
+</head>
+<body background="img/nativas/IS.jpg">
+   
+    
+</body>
+</html>
