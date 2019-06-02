a6cfe786dca0341df51b1090b9ac791769c4ada5
israel Heynes <israellh2001@hotmail.com>
2019-06-02 18:48:36 -0500

    Paypal agregado

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const path = require('path')
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const paypal = require('paypal-rest-sdk')
 
 const app = express()
+
+paypal.configure( {
+  'mode': 'sandbox',
+  'client_id': 'AUBeUO3hhNwBfx06DocOdGYDz1rHw-0mINP6x8_wigHy-OozNgfs45BRS2W0NlLr1GThXqpB-oUctQbA',
+  'client_secret': 'EARzYgeSdykurEpMj3UVesNPenge36fkepwyYgFLqyli2K_gkh4ZDUMxXtyhYljZnvU_C8zlGH-tiyNZ'
+});
 
 mongoose.connect('mongodb://root:MfuZZZBLXShFyu2@ds059672.mlab.com:59672/camrasstore', { useNewUrlParser: true})
   .then(db => console.log('db connected'))
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,55 @@
 const express = require('express')
 const router = express.Router()
+var paypal = require('paypal-rest-sdk');
 
 const productoControllers = require('../controllers/database/productos');
 const controllers = require('../controllers/index')
+
+router.get('/pay/:price', (req, res) => {
+    let price = req.params.price
+
+    var create_payment_json = {
+        "intent": "authorize",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/",
+            "cancel_url": "http://localhost:3000/"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "Carrito",
+                    "sku": "ShoppingCart",
+                    "price": price,
+                    "currency": "MXN",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "currency": "MXN",
+                "total": price
+            },
+            "description": "La compra de tu carrito"
+        }]
+    };
+
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            for (var index = 0; index < payment.links.length; index++) {
+                if (payment.links[index].rel === 'approval_url') {
+                    res.json(payment.links[index])
+                }
+            }
+        }
+    });
+    
+    
+})
  
 router.get('/', controllers.index)
 router.get('/Productos/:type', productoControllers.Productos)
